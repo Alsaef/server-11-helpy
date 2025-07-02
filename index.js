0c1568d8c07d58dcb43a8327a0c5c855ac9a51f3
Alsaef <saef.ratul@gmail.com>
2025-07-02 17:03:35 +0600

    dev

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: ['http://localhost:5173', 'https://b11-a11-helpy.netlify.app','https://b11-a11.vercel.app']
+}));
+
 
 
 const verifyJWT = (req, res, next) => {
